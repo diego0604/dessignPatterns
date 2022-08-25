438ffec16b52856e5b5d1398807a14b3fad75598
Diego Alejandro Salazar Ramirez <dietuspr@gmail.com>
2022-08-25 15:51:52 -0500

    Agregar archivos de proyecto.

---
+++ DesignPattern/DependencyInjectionPattern/Beer.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace DesignPattern.DependencyInjectionPattern
+{
+    public class Beer
+    {
+        private readonly string _name;
+        private readonly string _brand;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public Beer(string name, string brand, int a)
+        {
+            _name = name;
+            _brand = brand;
+        }
+    }
+}
